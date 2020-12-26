5e1e7549343588b307c70fdbfe951b1d38678451
Success Chukwu <iamcusicon@gmail.com>
2020-12-26 21:15:05 +0100

    Trying another way for PWA

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,19 @@
 class Navigate {
   constructor() {
-    this.exec();
+    this.exec()
   }
 
   loadCon(selector) {
-    $(".sectionCon").attr("hidden", "");
-    $(selector).removeAttr("hidden");
+    $(".sectionCon").attr("hidden", "")
+    $(selector).removeAttr("hidden")
   }
 
   dest(title = "", url, selector, extras = {}) {
     const handleExtras = (extras) => {
-      const { isClicked, isAuthPage } = extras;
+      const { isClicked, isAuthPage } = extras
       if (isClicked != undefined && isClicked == true) {
-        console.log("isClicked: ", isClicked);
-        history.pushState({ title, url, selector }, title, url);
+        console.log("isClicked: ", isClicked)
+        history.pushState({ title, url, selector }, title, url)
       }
       // if (isAuthPage != undefined && isAuthPage == true) {
       //   $("header.header").attr("hidden", "");
@@ -21,38 +21,38 @@ class Navigate {
       // }
 
       // Scroll to the top of the page.
-      window.scrollTo(9999, 0);
-    };
+      window.scrollTo(9999, 0)
+    }
 
     const changeTitle = (title) => {
-      var originalTitle = document.title;
+      var originalTitle = document.title
       var newTitle = `${title != "" ? title + " -- " : ""}${
         originalTitle.includes("--")
           ? originalTitle.split("--")[1]
           : originalTitle
-      }`;
-      document.title = newTitle;
-    };
+      }`
+      document.title = newTitle
+    }
 
     const mainMethod = () => {
-      handleExtras(extras);
-      changeTitle(title);
-      this.loadCon(selector);
-    };
+      handleExtras(extras)
+      changeTitle(title)
+      this.loadCon(selector)
+    }
 
     setTimeout(() => {
-      mainMethod();
-    }, 150);
+      mainMethod()
+    }, 150)
   }
 
   loadPartials() {
-    var partialCons = document.querySelectorAll("[data-partial]");
+    var partialCons = document.querySelectorAll("[data-partial]")
     partialCons.forEach((partialCon) => {
       var partialName = partialCon.dataset.partial,
         template = document.querySelector(`#${partialName}-partial`),
-        partialDoc = template.content.cloneNode(true);
-      partialCon.append(partialDoc);
-    });
+        partialDoc = template.content.cloneNode(true)
+      partialCon.append(partialDoc)
+    })
   }
 
   // Changes url, if user click back or forward
@@ -60,98 +60,106 @@ class Navigate {
     onpopstate = (e) => {
       if (e.state != null) {
         if (location.href.split(location.origin)[1] == e.state.url) {
-          this.loadCon(e.state.selector);
+          this.loadCon(e.state.selector)
         }
       } else {
-        this.handleUrls(location.href.split(location.origin)[1]);
+        this.handleUrls(location.href.split(location.origin)[1])
       }
-    };
+    }
   }
 
   // Handles every url
   handleUrls(baseUrl) {
     switch (baseUrl) {
       case "/":
-        this.dest("", baseUrl, "#homeCon");
-        break;
+        this.dest("", baseUrl, "#homeCon")
+        break
       case "/?/pricing":
-        this.dest("Pricing", baseUrl, "#pricingCon");
-        break;
+        this.dest("Pricing", baseUrl, "#pricingCon")
+        break
       case "/?/demo":
-        this.dest("Demo", baseUrl, "#demoCon");
-        break;
+        this.dest("Demo", baseUrl, "#demoCon")
+        break
       case "/?/auth/signup":
         this.dest("Signup", baseUrl, "#signupCon", {
           isClicked: false,
           isAuthPage: true,
-        });
-        break;
+        })
+        break
       case "/?/auth/login":
         this.dest("Login", baseUrl, "#loginCon", {
           isClicked: false,
           isAuthPage: true,
-        });
-        break;
+        })
+        break
 
       default:
         // this.dest("", baseUrl, "#homeCon");
-        break;
+        break
     }
   }
 
   fixedBottomFooter() {
     if ($(".site-wrap").height() < window.innerHeight) {
-      $("footer.footer").addClass("fixed-bottom");
+      $("footer.footer").addClass("fixed-bottom")
     } else {
-      $("footer.footer").removeClass("fixed-bottom");
+      $("footer.footer").removeClass("fixed-bottom")
     }
   }
 
   exec() {
     document.onreadystatechange = () => {
       if (document.readyState === "complete") {
-        this.loadPartials();
-        this.handleUrls(location.href.split(location.origin)[1]);
-        this.handleUrlChange();
+        this.loadPartials()
+        this.handleUrls(location.href.split(location.origin)[1])
+        this.handleUrlChange()
         setTimeout(() => {
-          $("body").removeAttr("hidden");
-          this.fixedBottomFooter();
-        }, 200);
+          $("body").removeAttr("hidden")
+          this.fixedBottomFooter()
+        }, 200)
       }
       if (document.readyState === "interactive") {
-        $("body").attr("hidden", "");
+        $("body").attr("hidden", "")
       }
-    };
+    }
 
     window.onresize = () => {
-      this.fixedBottomFooter();
-    };
+      this.fixedBottomFooter()
+    }
   }
 }
 
 class Router {
   constructor() {
-    this.exec();
+    this.exec()
   }
 
   gotoRoute() {
     $("[data-href]").click((e) => {
-      var title = e.currentTarget.dataset.title;
-      var href = e.currentTarget.dataset.href;
-      var hrefcon = e.currentTarget.dataset.hrefcon;
-      console.log(title, href, hrefcon);
+      var title = e.currentTarget.dataset.title
+      var href = e.currentTarget.dataset.href
+      var hrefcon = e.currentTarget.dataset.hrefcon
+      console.log(title, href, hrefcon)
 
       nav.dest(title, href, hrefcon, {
         isClicked: true,
         isAuthPage: true,
-      });
-    });
+      })
+    })
   }
 
   exec() {
-    this.gotoRoute();
+    this.gotoRoute()
   }
 }
 
-const nav = new Navigate();
-new Router();
+const nav = new Navigate()
+// new Router()
+
+function PWA() {
+  if ("serviceWorker" in navigator) {
+    // register service worker
+    navigator.serviceWorker.register("service-worker.js")
+  }
+}
+PWA()
